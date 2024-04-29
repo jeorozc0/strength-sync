482ffcde6cdc473e2599dc891d7fd8fd4d81a0d2
jeorozc0 <jeorozco@bsu.edu>
2024-04-29 00:58:40 -0400

    Update workout-tracker-dialog.tsx to use FitnessCenterIcon instead of PersonIcon and update routing link

--- src/components/workout-tracker/workout-tracker-dialog.tsx
+++ src/components/workout-tracker/workout-tracker-dialog.tsx
@@ -6,10 +6,10 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
-import PersonIcon from "@mui/icons-material/Person";
 import AddIcon from "@mui/icons-material/Add";
 import { blue } from "@mui/material/colors";
 import { useNavigate } from "react-router-dom";
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 type SimpleDialogProps = {
   onClose: () => void;
@@ -25,20 +25,20 @@ const SimpleDialog = ({ onClose, open, workouts }: SimpleDialogProps) => {
 
   const handleListItemClick = (workout: any) => {
     console.log("Editing workout with id: " + workout.workout_id);
-    navigate(`/edit-workout/${workout.workout_id}`);
+    navigate(`/tracker/workout/${workout.workout_id}`);
     onClose();
   };
 
   return (
     <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
+      <DialogTitle>Select a Workout</DialogTitle>
       <List sx={{ pt: 0 }}>
         {workouts?.map((workout: any) => (
           <ListItem disableGutters key={workout.workout_id}>
             <ListItemButton onClick={() => handleListItemClick(workout)}>
               <ListItemAvatar>
                 <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                  <PersonIcon />
+                  <FitnessCenterIcon />
                 </Avatar>
               </ListItemAvatar>
               <ListItemText primary={workout.workout_name} />
