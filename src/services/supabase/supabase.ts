22e5b0754614a2451b80c7e22d55e4fba7d0848e
jeorozc0 <jeorozco@bsu.edu>
2024-06-04 13:24:56 -0400

    Refactor supabase.ts to use NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY for environment variables

--- src/services/supabase/supabase.ts
+++ src/services/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_APP_URL;
-const supabaseKey = import.meta.env.VITE_APP_ANON_KEY;
+const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
