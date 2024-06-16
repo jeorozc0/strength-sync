2527e861ba3be9c75bb8a9314fe666003ccf4a08
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-06-16 09:39:24 -0700

    Add RoutineCreateWithAI component to RoutineCreate

---
+++ src/components/user-workouts/workout-create-ai.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import WorkoutCreateAIForm from "./workout-create-ai-form";
+
+const RoutineCreateWithAI = () => {
+  return (
+    <>
+
+      <WorkoutCreateAIForm />
+    </>
+  );
+};
+
+export default RoutineCreateWithAI;
--- src/components/user-workouts/workout-create.tsx
+++ src/components/user-workouts/workout-create.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import RoutineCreateWithAI from "./workout-create-ai";
 
 const RoutineCreate = () => {
   return (
-    <Link
-      to={"/create-routine"}
-      className="flex gap-5 flex-col border-[#ECEDF0] border border-solid bg-white h-max w-11/12 lg:w-72 rounded-md p-6"
-    >
-      <div className=" flex align-middle justify-center  border-[#ECEDF0] border border-solid bg-white hover:bg-[#F9FAFB] rounded-md cursor-pointer lg:p-3 p-5">
+    <div className="flex gap-5 flex-col border-[#ECEDF0] border border-solid bg-white h-max w-11/12 lg:w-72 rounded-md p-6">
+      <Link
+        to={"/create-routine"}
+        className=" flex align-middle justify-center  border-[#ECEDF0] border border-solid bg-white hover:bg-[#F9FAFB] rounded-md cursor-pointer lg:p-3 p-5"
+      >
         <p className="font-medium text-sm text-center">New Routine</p>
-      </div>
-    </Link>
+      </Link>
+      <RoutineCreateWithAI />
+      
+    </div>
   );
 };
 
