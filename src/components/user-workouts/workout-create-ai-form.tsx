c78908a3780fdba44719e208e0a0dda196d11e7d
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-06-18 18:04:41 -0700

    improve code readability and remove unnecessary overflow-scroll in layout component

--- src/components/user-workouts/workout-create-ai-form.tsx
+++ src/components/user-workouts/workout-create-ai-form.tsx
@@ -26,6 +26,17 @@ export default function WorkoutCreateAIForm() {
     setOpen(false);
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const formJson = Object.fromEntries(formData.entries());
+    const muscle = formJson.muscle;
+    const exercises = formJson.exercises;
+    const sets = formJson.sets;
+    console.log(muscle, exercises, sets);
+    handleClose();
+  };
+
   return (
     <React.Fragment>
       <button
@@ -39,105 +50,94 @@ export default function WorkoutCreateAIForm() {
         onClose={handleClose}
         PaperProps={{
           component: CustomPaper,
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-            const muscle = formJson.muscle;
-            const exercises = formJson.exercises;
-            const sets = formJson.sets;
-            console.log(muscle);
-            console.log(exercises);
-            console.log(sets);
-            handleClose();
-          },
         }}
       >
         <DialogTitle sx={{ display: "flex", justifyContent: "center" }}>
           Workout Generation
         </DialogTitle>
-        <DialogContent>
-          <div className="flex gap-6 flex-col">
-            {" "}
-            <div className="flex gap-4">
-              <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  Muscle Group
-                </InputLabel>
-                <NativeSelect
-                  title="Muscle"
-                  defaultValue={""}
-                  inputProps={{
-                    name: "muscle",
-                    id: "uncontrolled-native",
-                  }}
-                >
-                  <option value={"Chest"}>Chest</option>
-                  <option value={"Back"}>Back</option>
-                  <option value={"Legs"}>Legs</option>
-                  <option value={"Shoulder"}>Shoulder</option>
-                  <option value={"Biceps"}>Biceps</option>
-                  <option value={"Triceps"}>Triceps</option>
-                  <option value={"Calves"}>Calves</option>
-                  <option value={"Forearms"}>Forearms</option>
-                  <option value={"Core"}>Core</option>
-                </NativeSelect>
-              </FormControl>
+        <form onSubmit={handleSubmit}>
+          <DialogContent>
+            <div className="flex gap-6 flex-col">
+              <div className="flex gap-4">
+                <FormControl fullWidth>
+                  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                    Muscle Group
+                  </InputLabel>
+                  <NativeSelect
+                    title="Muscle"
+                    defaultValue={""}
+                    inputProps={{
+                      name: "muscle",
+                      id: "uncontrolled-native",
+                    }}
+                  >
+                    <option value={"Chest"}>Chest</option>
+                    <option value={"Back"}>Back</option>
+                    <option value={"Legs"}>Legs</option>
+                    <option value={"Shoulder"}>Shoulder</option>
+                    <option value={"Biceps"}>Biceps</option>
+                    <option value={"Triceps"}>Triceps</option>
+                    <option value={"Calves"}>Calves</option>
+                    <option value={"Forearms"}>Forearms</option>
+                    <option value={"Core"}>Core</option>
+                  </NativeSelect>
+                </FormControl>
+              </div>
+              <div className="flex gap-4">
+                <FormControl fullWidth>
+                  <InputLabel
+                    variant="standard"
+                    htmlFor="uncontrolled-native-exercises"
+                  >
+                    Number of exercises
+                  </InputLabel>
+                  <NativeSelect
+                    title="Exercises"
+                    defaultValue={4}
+                    inputProps={{
+                      name: "exercises",
+                      id: "uncontrolled-native-exercises",
+                    }}
+                  >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                  </NativeSelect>
+                </FormControl>
+                <FormControl fullWidth>
+                  <InputLabel
+                    variant="standard"
+                    htmlFor="uncontrolled-native-sets"
+                  >
+                    Sets per exercise
+                  </InputLabel>
+                  <NativeSelect
+                    title="sets"
+                    defaultValue={2}
+                    inputProps={{
+                      name: "sets",
+                      id: "uncontrolled-native-sets",
+                    }}
+                  >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                  </NativeSelect>
+                </FormControl>
+              </div>
             </div>
-            <div className="flex gap-4">
-              <FormControl fullWidth>
-                <InputLabel
-                  variant="standard"
-                  htmlFor="uncontrolled-native-exercises"
-                >
-                  Number of exercises
-                </InputLabel>
-                <NativeSelect
-                  title="Exercises"
-                  defaultValue={4}
-                  inputProps={{
-                    name: "exercises",
-                    id: "uncontrolled-native-exercises",
-                  }}
-                >
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                  <option value={5}>5</option>
-                  <option value={6}>6</option>
-                  <option value={7}>7</option>
-                  <option value={8}>8</option>
-                </NativeSelect>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel
-                  variant="standard"
-                  htmlFor="uncontrolled-native-sets"
-                >
-                  Sets per exercise
-                </InputLabel>
-                <NativeSelect
-                  title="sets"
-                  defaultValue={2}
-                  inputProps={{
-                    name: "sets",
-                    id: "uncontrolled-native-sets",
-                  }}
-                >
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                </NativeSelect>
-              </FormControl>
-            </div>
-          </div>
-        </DialogContent>
-        <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Generate</Button>
-        </DialogActions>
+          </DialogContent>
+          <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Generate</Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </React.Fragment>
   );
