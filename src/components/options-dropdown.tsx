dd8323ca30c55c7d68bf6019d5b96cecb044037b
jeorozc0 <jeorozco@bsu.edu>
2024-06-27 20:03:39 -0400

    Refactor OptionsDropdown component to include better information

--- src/components/options-dropdown.tsx
+++ src/components/options-dropdown.tsx
@@ -6,35 +6,42 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 import { ThemeToggle } from "./ui/theme-toggle";
 import { useTheme } from "../hooks/useTheme";
 import { useAuth } from "../hooks/useAuth";
 export function OptionsDropdown() {
   const { darkMode, setDarkMode } = useTheme();
-  const { signOut } = useAuth();
+  const { user, signOut } = useAuth();
+  const userEmail = user?.email;
+  const avatarFallback = userEmail?.charAt(0).toUpperCase();
 
   const handleLogout = () => {
     signOut();
   };
 
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarFallback>{avatarFallback}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="dark:bg-[#1A1A1C]">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuLabel className="gap-8 flex flex-row items-center font-normal">
           Theme <ThemeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
         </DropdownMenuLabel>
-        <DropdownMenuItem className="h-10">Billing</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleLogout} className="cursor-pointer h-10">Log out</DropdownMenuItem>
+        <DropdownMenuItem className="h-10" disabled>
+          Billing
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={handleLogout}
+          className="cursor-pointer h-10"
+        >
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
