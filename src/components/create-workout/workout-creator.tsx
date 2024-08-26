285f531123ac9bc566fefd7eb67bce00b3fd1c01
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-25 21:41:52 -0400

    move AI generation button

--- src/components/create-workout/workout-creator.tsx
+++ src/components/create-workout/workout-creator.tsx
@@ -5,8 +5,6 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import ExerciseCreatorItem from "./exercise-creator-item";
-import WorkoutCreateAIForm from "../user-workouts/workout-create-ai-form";
-import { useCreateWorkoutWithAi } from "../../hooks/useExercise";
 import { ExcersiceListModal } from "../exercise-list-modal";
 
 interface WorkoutEditorProps {
@@ -25,14 +23,12 @@ const WorkoutCreator = ({
   addExercise,
 }: WorkoutEditorProps) => {
   const [routineName, setRoutineName] = useState("My Routine");
-  const { mutateAsync: createWorkoutWithAi, isLoading } =
-    useCreateWorkoutWithAi();
 
   const updateExerciseDetails = (
     exercise_id: string,
     sets: number,
     reps: number,
-    rest: number
+    rest: number,
   ) => {
     const updated: EditExerciseProps[] | undefined = exercises?.map(
       (subArray) => {
@@ -45,24 +41,10 @@ const WorkoutCreator = ({
           };
         }
         return subArray;
-      }
+      },
     );
     setLocalExerciseDetails(updated);
   };
-
-  async function createAi({ muscle, exercises, sets }: any) {
-    await createWorkoutWithAi({
-      muscle: muscle,
-      exercises: exercises,
-      sets: sets,
-    })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
 
   return (
     <div className="flex flex-col w-full">
@@ -76,10 +58,6 @@ const WorkoutCreator = ({
           </h1>
         </div>
         <div className="flex flex-row gap-4">
-          <WorkoutCreateAIForm
-            createWorkoutWithAi={createAi}
-            isLoading={isLoading}
-          />
           <Button
             variant="contained"
             size="small"
@@ -103,20 +81,18 @@ const WorkoutCreator = ({
       />
       {exercises.length > 0 ? (
         <>
-          {exercises.map((exercise: any) => {
-            return (
-              <ExerciseCreatorItem
-                key={exercise.exercises.exercise_id}
-                exercise_name={exercise.exercises.exercise_name}
-                exercise_id={exercise.exercises.exercise_id}
-                sets={exercise.sets}
-                reps={exercise.reps}
-                rest={exercise.rest}
-                removeExercise={deleteExerciseNow}
-                updateExerciseDetails={updateExerciseDetails}
-              />
-            );
-          })}
+          {exercises.map((exercise: any) => (
+            <ExerciseCreatorItem
+              key={exercise.exercises.exercise_id}
+              exercise_name={exercise.exercises.exercise_name}
+              exercise_id={exercise.exercises.exercise_id}
+              sets={exercise.sets}
+              reps={exercise.reps}
+              rest={exercise.rest}
+              removeExercise={deleteExerciseNow}
+              updateExerciseDetails={updateExerciseDetails}
+            />
+          ))}
         </>
       ) : (
         <div className="flex justify-center items-center flex-col h-auto border-[#ECEDF0] dark:border-black border border-solid bg-[#FFFFFF] dark:bg-[#2B2C32] rounded-md p-10 gap-5 mb-6">
