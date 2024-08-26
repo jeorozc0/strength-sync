7d304d6e1ff11bf173eafa160d2cbad60a10442c
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-25 21:41:06 -0400

    add types for generated workouts

--- src/types/workout-types.ts
+++ src/types/workout-types.ts
@@ -1,8 +1,27 @@
 export interface WorkoutProps {
   workout_id?: string;
   workout_name?: string;
-  user_id?: any
-  session_id?: any
+  user_id?: any;
+  session_id?: any;
   workout_session_id?: any;
-  date?: any
+  date?: any;
+}
+
+export interface Exercise {
+  exercise_name: string;
+  exercise_id: string;
+  sets: string; // Note: This is a string, not a number
+}
+
+export interface GeneratedWorkout {
+  muscle: string;
+  exercises: Exercise[];
+}
+
+export interface WorkoutState {
+  generatedWorkout: GeneratedWorkout | null;
+  isFromAI: boolean;
+  setGeneratedWorkout: (workout: GeneratedWorkout) => void;
+  setIsFromAI: (value: boolean) => void;
+  clearWorkoutState: () => void;
 }
